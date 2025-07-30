92c0aa8ba16e6138716554fcfc8eb296e8af19fe
Abel Cota <abelc@tejassoftware.com>
2025-07-30 15:26:00 -0600

    Fixing client information relationship

--- src/app/models/environment.model.ts
+++ src/app/models/environment.model.ts
@@ -2,23 +2,4 @@ export interface EnvironmentModel {
     production: boolean;
     version: string;
     apiBaseUrl: string;
-    // twmsApiUrl: string;
-    // twmsApiVersionHeader: string;
-    // twmsApiVersion: string;
-    // oldTwmsRoute: string;
-    // apiTimeoutMs: number;
-    // debounceTimeMS: number;
-    // stageType: StageType;
-    // availableLangs: LangDefinition[];
-    // defaultLang: TranslationLanguage;
-    // fallbackLang: TranslationLanguage,
-    // dateFormat: string;
-    // dateTimeFormat: string;
-    // checkAuthIntervalMs: number;
-    // recaptcha: RecaptchaEnvironmentSettings;
-    // baseHref: string;
-    // defaultTimeZone: string;
-    // localStorageKeys: LocalStorageKeys;
-    // saveNotificationsInStorage: boolean;
-    // what3WordsUrl: string;
 }
