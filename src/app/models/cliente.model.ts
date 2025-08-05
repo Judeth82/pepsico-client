d43e5a10de95d21d7db444b16bf9a34ba552f936
Abel Cota <abelc@tejassoftware.com>
2025-08-05 11:17:37 -0600

    Update client data

--- src/app/models/cliente.model.ts
+++ src/app/models/cliente.model.ts
@@ -1,16 +1,7 @@
 export interface ClienteModel {
+  nipContact: string;
   id: string,
   nombre: string;
   distrito: string;
-  ciaInase: string;
-  division: string;
-  cedis: string;
-  rutasLegado: string;
-  rutaSAP: string;
-  nivelCliente: string;
-  semana: string;
-  ventaHHCCapt: number;
-  visitasRealizadasSinEventuales: number;
-  visitasProgramadas: number;
-  clientesVenta: number;
+  sap: string
 }
