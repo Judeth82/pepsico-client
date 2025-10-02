e4c71e4b2ef07231386ab9b97d72973e947e30c0
Abel Cota <abelc@tejassoftware.com>
2025-10-02 13:57:14 -0600

    [RM-PepsicoClient] Change Logo and Style

--- src/environments/environment.demo.ts
+++ src/environments/environment.demo.ts
@@ -3,5 +3,5 @@ import { EnvironmentModel } from '@m/environment.model';
 export const environment: EnvironmentModel = {
   production: false,
   version: 'Demo 1.0.0',
-  apiBaseUrl: 'https://pepsico-service-75bf21dd4606.herokuapp.com/'
+  apiBaseUrl: 'https://pepsico-service-650811d77b5f.herokuapp.com/'
 };
